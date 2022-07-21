b6e93a8538ba742d280d1acc096f4b5bf6cebd6e
김무선 <96815572+KimLineless@users.noreply.github.com>
2022-07-21 12:11:17 +0900

    test

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,13 +1,25 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import style from '../detail.module.css';
 
 function Detail(props) {
   let { id } = useParams();
   id = Number(id);
+
+  useEffect(() => {});
+
   return (
     <div className='list_con'>
       <p>{props.shoes[id].title}</p>
-      <img alt='' src={`${process.env.PUBLIC_URL}/img/product0${id + 1}.jpg`} />
+      <div class='img'>
+        <img
+          alt=''
+          src={`${process.env.PUBLIC_URL}/img/product0${id + 1}.jpg`}
+        />
+      </div>
       <p>{props.shoes[id].price}</p>
+      <p>{props.shoes[id].txt}</p>
     </div>
   );
 }
